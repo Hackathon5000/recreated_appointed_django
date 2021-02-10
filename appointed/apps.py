cce9faafd1c630b5b8074acb4ab38071c55bc060
shalomjlee <shalomjlee@gmail.com>
2021-02-10 13:53:20 -0600

    recreated working backend

---
+++ appointed/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppointedConfig(AppConfig):
+    name = 'appointed'
---
+++ settings.sql
@@ -0,0 +1,4 @@
+-- settings.sql
+CREATE DATABASE appointed2;
+CREATE USER appointed2user WITH PASSWORD 'appointed2';
+GRANT ALL PRIVILEGES ON DATABASE appointed2 TO appointed2user;
